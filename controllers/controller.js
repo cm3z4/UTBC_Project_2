9f68be2785d6dbc1bc40c65d43bb8bf7a60e114b
riffon3000 <riffon@gmail.com>
2018-12-02 13:11:40 -0600

    file structure

---
+++ controllers/controller.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the model (index.js) to use its database functions.
+const index = require("../models/index.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  index.all(function (data) {
+    let hbsObject = {
+      index: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+// NEED TO UPDATE ???? WITH DETERMINED SCHEMA
+// router.post("/api/index", function(req, res) {
+//   index.create(["????", "?????"], [req.body.????, req.body.?????], function(result) {
+//     // Send back the ID of the new quote
+//     res.json({ id: result.insertId });
+//   });
+// });
+
+// router.put("/api/index/:id", function(req, res) {
+//   let condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   index.update(
+//     {
+//       ?????: req.body.?????
+//     },
+//     condition,
+//     function(result) {
+//       if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+//     }
+//   );
+// });
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/controller.js");
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
