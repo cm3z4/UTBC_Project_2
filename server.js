06471ba9d4e1cced7d43f10b73e40e2fc54a79dc
riffon3000 <riffon@gmail.com>
2018-12-05 22:40:32 -0600

    let vs const

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-let bodyParser = require("body-parser");
-let express = require("express");
-let path = require("path");
+const express = require("express");
+const path = require("path");
 
-let app = express();
-let PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "./public")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-let exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let router = require("./controllers/controller.js");
+const router = require("./controllers/controller.js");
 app.use("/", router);
 
 // Application is listening...
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("Application is listening on PORT " + PORT + ".");
 });
