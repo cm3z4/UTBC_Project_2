5d92ab788a9106ae7a098e03354342bc174049cf
SultanaK <kr.shampa@yahoo.com>
2018-12-11 20:40:15 -0600

    adding css to login and signup

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE things4sale_db;
-USE things4sale_db;
-
--- Create burgers table.
-CREATE TABLE users(
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
-user VARCHAR(100) NOT NULL,
-pass VARCHAR(100) NOT NULL);
